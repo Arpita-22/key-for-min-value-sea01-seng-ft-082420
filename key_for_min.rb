4a21aadff4c560b9a8d999f4ccb16ff8c45fec09
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-23 20:49:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+  find_min = name_hash.keys[0]
+  name_hash.each do |key,value|
+    if value<find_min
+      find_min = value
+      end
+    end
+    name_hash.key(find_min)
+ end
